3cf3629a74a0983eadd40b3cd94c9d513a63d98e
Echooff3 <john.skibicki.iii@gmail.com>
2015-09-03 00:11:37 -0500

    Inital commit

---
+++ HeadsUpClone/HeadsUpClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeadsUpClone
+//
+//  Created by Skibicki III, John (CHI-FCB) on 9/3/15.
+//  Copyright (c) 2015 Skibicki III, John (CHI-FCB). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
